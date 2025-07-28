dd7a61d35f387ca7293ec171088bbb30dd346b67
rabent <72433154+rabent@users.noreply.github.com>
2025-07-28 18:01:18 +0900

    <feat> : entity 수정

---
+++ demo/src/main/java/com/example/demo/auth/dto/UserProfileRequest.java
@@ -0,0 +1,37 @@
+package com.example.demo.auth.dto;
+
+import com.example.demo.auth.Enum.PersonalPrefEnum;
+import com.example.demo.auth.Enum.ProjectPrefEnum;
+import com.example.demo.auth.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileRequest {
+    private String major;
+    private Integer lastClass;
+    private String wantedPosition;
+    private String projectPref;
+    private String personalPref;
+    private String projectExp;
+    private String qualification;
+    private String techStack;
+
+    public static User toEntity(UserProfileRequest userProfileRequest) {
+        User user = new User();
+        user.setMajor(userProfileRequest.getMajor());
+        user.setLastClass(userProfileRequest.getLastClass());
+        user.setWantedPosition(userProfileRequest.getWantedPosition());
+        user.setPersonalPref(PersonalPrefEnum.valueOf(userProfileRequest.getPersonalPref()));
+        user.setProjectPref(ProjectPrefEnum.valueOf(userProfileRequest.getProjectPref()));
+        user.setProjectExp(userProfileRequest.getProjectExp());
+        user.setQualification(userProfileRequest.getQualification());
+        user.setTechStack();
+    }
+
+}
--- demo/src/main/java/com/example/demo/auth/entity/User.java
+++ demo/src/main/java/com/example/demo/auth/entity/User.java
@@ -1,6 +1,11 @@
 package com.example.demo.auth.entity;
 
+import com.example.demo.auth.Enum.PersonalPrefEnum;
+import com.example.demo.auth.Enum.ProjectPrefEnum;
+import com.example.demo.auth.Enum.TechEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,40 +18,88 @@ import java.util.Set;
 @Getter
 @Setter
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "USER_ID")
+    @Column(name = "user_id")
     private Long id;
 
-    private String username;
+    @Column(name="user_name", length = 10, nullable = false)
+    @NotBlank
+    @Size(max = 10)
+    private String userName;
 
     @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
+    @JoinColumn(name = "team_id")
     private Team team;
 
-    @Column(unique = true)
+    @Column(name= "user_email",unique = true, nullable = false, length = 50)
+    @NotBlank
+    @Size(max = 50)
     private String email;
-    private String role;
 
-    @ManyToMany
-    @JoinTable(
-            name = "USER_TECH_STACK", // 중간 테이블 이름
-            joinColumns = @JoinColumn(name = "USER_ID"),       // 현재 Entity(User)의 FK
-            inverseJoinColumns = @JoinColumn(name = "TECH_STACK_ID") // 연결될 Entity(TechStack)의 FK
-    )
-    private Set<TechStack> techStacks = new HashSet<>();
+    @Column(name="user_profile", length = 550)
+    @Size(max = 550)
+    private String userProfile;
+
+    //private String projectStep;
+
+    @Column(name = "major",  nullable = false, length = 5)
+    @Size(max = 5)
+    @NotBlank
+    private String major;
+
+    @Column(name = "last_class", nullable = false)
+    @NotBlank
+    private Integer lastClass;
+
+    @Column(name = "wanted_postion",  nullable = false,  length = 50)
+    @NotBlank
+    @Size(max = 50)
+    private String wantedPosition;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "project_preference", nullable = false, length = 50)
+    @NotBlank
+    @Size(max = 50)
+    private ProjectPrefEnum projectPref;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "personal_preference", nullable = false, length = 50)
+    @NotBlank
+    @Size(max = 50)
+    private PersonalPrefEnum personalPref;
+
+    @Column(name = "project_experience", nullable = true, length = 300)
+    @Size(max = 300)
+    private String projectExp;
+
+    @Column(name="qualification", nullable = true, length = 45)
+    @Size(max = 45)
+    private String qualification;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="tech_stack",  nullable = true, length = 100)
+    @Size(max = 100)
+    private TechEnum techStack;
+
+    //@ManyToMany
+    //@JoinTable(
+    //       name = "USER_TECH_STACK", // 중간 테이블 이름
+    //        joinColumns = @JoinColumn(name = "USER_ID"),       // 현재 Entity(User)의 FK
+    //        inverseJoinColumns = @JoinColumn(name = "TECH_STACK_ID") // 연결될 Entity(TechStack)의 FK
+    //)
+    //private Set<TechStack> techStacks = new HashSet<>();
 
     public void setTeam(Team team) {
         this.team = team;
         if (!team.getUsers().contains(this)) {
             team.getUsers().add(this);
-        }
+       }
     }
 
-    public void addTechStacks(TechStack techStack) {
-        techStacks.add(techStack);
-    }
+    //public void addTechStacks(TechStack techStack) {
+    //    techStacks.add(techStack);
+    //}
 
     @Override
     public boolean equals(Object o) {
