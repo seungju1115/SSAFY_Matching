03e639f7580f1984959c3df0f2d790945fa20357
DESKTOP-RBNO6LS\SSAFY <coco970@naver.com>
2025-08-08 17:20:13 +0900

    feat : 팀페이지 추천 ui

---
+++ front_end/src/components/features/team/recommendation/index.ts
@@ -0,0 +1,3 @@
+export { default as ProfileRecommendModal } from './ProfileRecommendModal';
+export { default as RecommendedProfileCard } from './RecommendedProfileCard';
+export type { ProfileData } from './RecommendedProfileCard';
