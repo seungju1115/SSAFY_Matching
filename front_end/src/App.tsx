2f2e343555f6f248f07673a02c4244d58b667954
이승주 <joo4123@naver.com>
2025-08-10 21:12:14 +0900

    feat(chat) : 채팅용 웹소켓 연결 프론트버전 추가

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,28 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Setup from './pages/Setup'
-import ProfileSetup from './pages/ProfileSetup'
-import MakeTeam from './pages/MakeTeam/make'
-import AuthCallback from './pages/Auth/Callback'
-import Team from './pages/Team'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/setup" element={<Setup />} />
-        <Route path="/profile-setup" element={<ProfileSetup />} />
-        <Route path="/make-team" element={<MakeTeam />} />
-        <Route path="/oauth/callback" element={<AuthCallback />} />
-        <Route path="/team/:teamId" element={<Team />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App 
+export default App;
