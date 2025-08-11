78032512147e725cd99f27b95b740fe21cf47ece
이승주 <joo4123@naver.com>
2025-08-11 13:07:27 +0900

    fix(team) : websocket 연결 cors, socket.js 코드 오류 수정

--- back_end/src/main/java/com/example/demo/auth/config/CorsConfig.java
+++ back_end/src/main/java/com/example/demo/auth/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,13 +13,15 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${front.url}")
+    private String frontUrl;
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of(frontUrl));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
-
+        config.setAllowCredentials(true);
         return request -> config;
     }
 }
