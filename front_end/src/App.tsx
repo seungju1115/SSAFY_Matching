d667513de0e07851f2804e307ae781f100e87cd4
봉현진 <coco970@naver.com>
2025-08-13 11:59:23 +0900

    feat : 개인페이지&대시보드 병합

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -8,6 +8,7 @@ import MakeTeam from './pages/MakeTeam/make'
 import AuthCallback from './pages/Auth/Callback'
 import Team from './pages/Team'
 import Chat from './pages/Chat'
+import Dashboard from './pages/Dashboard'
 import ProfileLayout from './pages/Profile/Layout'
 import ProfileInfo from './pages/Profile/Info'
 import ProfileTeam from './pages/Profile/Team'
@@ -26,6 +27,7 @@ function App() {
         <Route path="/oauth/callback" element={<AuthCallback />} />
         <Route path="/team/:teamId" element={<Team />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/profile" element={<ProfileLayout />}>
           <Route index element={<ProfileInfo />} />
           <Route path="team" element={<ProfileTeam />} />
