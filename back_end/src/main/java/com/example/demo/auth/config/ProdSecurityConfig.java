8a0886f840074d0177cf52a8f32f9fc2a1335011
이승주 <joo4123@naver.com>
2025-08-15 15:34:07 +0900

    fix(chat) : chat post 요청 허용

--- back_end/src/main/java/com/example/demo/auth/config/ProdSecurityConfig.java
+++ back_end/src/main/java/com/example/demo/auth/config/ProdSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.beans.factory.annotation.Value;
 
 @Profile("prod")
 @Configuration
