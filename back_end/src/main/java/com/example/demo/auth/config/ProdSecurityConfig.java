a241733532c1870712a609b5ee92b65d203d5563
이승주 <joo4123@naver.com>
2025-08-15 15:40:59 +0900

    fix(chat) : chat post 요청 허용

--- back_end/src/main/java/com/example/demo/auth/config/ProdSecurityConfig.java
+++ back_end/src/main/java/com/example/demo/auth/config/ProdSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.cors.CorsConfiguration;
 import java.util.List;
 
 @Profile("prod")
